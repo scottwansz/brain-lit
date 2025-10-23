bd2f3db346732efc8b5eaa0106b2729e52fd56d6
scottwan <scottwan@foxmail.com>
2025-10-23 13:12:30 +0800

    opt: exclude used ones when neutralize

--- tests/test_neutralize.py
+++ tests/test_neutralize.py
@@ -28,7 +28,7 @@ class TestNeutralize(unittest.TestCase):
         selected_alphas = query_by_sql( sql)
 
         # 排除best_alphas中used属性为1的记录
-        best_alphas = [alpha for alpha in selected_alphas if alpha.get('used') != 1]
+        selected_alphas = [alpha for alpha in selected_alphas if alpha.get('used') != 1]
 
         print('len(selected_alphas):', len(selected_alphas))
 
